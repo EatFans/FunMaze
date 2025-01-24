51a9c2f821839f6f459ff5e813f3305e197e0426
EatFan <eatfan0921@163.com>
2025-01-24 21:11:18 +0800

    添加怪物

--- script/level3.cs
+++ script/level3.cs
@@ -3,18 +3,18 @@ using System;
 
 public partial class level3 : Node2D
 {
-	// Called when the node enters the scene tree for the first time.
+
 	public override void _Ready()
 	{
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
+
 	public override void _Process(double delta)
 	{
 	}
 
 	public void _on_return_button_pressed()
 	{
-		GetTree().ChangeSceneToFile("res://scenes/ui/main_menu.tscn");
+		GetTree().ChangeSceneToFile("res://scenes/ui/main_menu.tscn")
 	}
 }
