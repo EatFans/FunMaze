fb98d354903f1376ae56a2fc844bf3e3f81f26c9
EatFan <eatfan0921@163.com>
2025-01-25 17:26:59 +0800

    简单完成一号怪物代码逻辑

--- script/monster1.cs
+++ script/monster1.cs
@@ -7,11 +7,13 @@ using System;
 public partial class monster1 : CharacterBody2D
 {
 	private Node2D player;  // 玩家节点
-	public const float Speed = 300.0f;
-	public float detectionRadius = 200.0f;  // 追击范围
+	public const float Speed = 100.0f;
+	public float detectionRadius = 120.0f;  // 追击范围
 	public const float JumpVelocity = -400.0f;
 
-	// Get the gravity from the project settings to be synced with RigidBody nodes.
+	private bool isPursue = false;  // 是否进入追击状态开关
+
+
 	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
 
     public override void _Ready()
@@ -20,13 +22,14 @@ public partial class monster1 : CharacterBody2D
 		var levelNode = GetTree().CurrentScene;
 		player = levelNode.GetNode<CharacterBody2D>("Player");
 		
-		if (player != null)
-			GD.Print("已经获取玩家节点："+player.Name);
+		if (player == null)
+			GD.Print("未获取玩家节点");
+
+		GD.Print("一号怪物已经加载");
 
     }
 
 
-    // TODO: 一号怪物开始时候是保持不动的，当玩家接近一号怪物指定距离的时候，一号怪物开始移动追击玩家，直到碰到玩家，重置当前场景
     public override void _PhysicsProcess(double delta)
 	{
 		if (player == null)
@@ -37,6 +40,40 @@ public partial class monster1 : CharacterBody2D
 		if (distanceToPlaye <= detectionRadius)
 		{
 			// 如果在追击的范围，开始追击玩家
+			isPursue = true;
+			// GD.Print("一号怪物开始追击玩家");
+		}
+		if (isPursue)
+		{
+			// 开始追击玩家
+			PursuePlayer(delta);
+		}
+
+		CheckCollisionWithPlayer();
+
+	}
+
+	private void PursuePlayer(double delta)
+    {
+        // 计算怪物追向玩家的方向（单位向量）
+        Vector2 directionToPlayer = (player.Position - Position).Normalized();
+
+        // 设置速度向量
+        Velocity = directionToPlayer * Speed;
+
+        // 调用 MoveAndSlide 来更新怪物的位置
+        MoveAndSlide();
+		// GD.Print("追击中...");
+    }
+
+	private void CheckCollisionWithPlayer()
+	{
+		// GD.Print("玩家距离怪物的距离"+Position.DistanceTo(player.Position));
+		if (Position.DistanceTo(player.Position) < 21.0f)
+		{
+			GD.Print("一号怪物已经触碰到玩家");
+			SoundPlayer.Instance.playTouchBlackTileSound();
+			GetTree().ReloadCurrentScene();
 		}
 	}
 }
